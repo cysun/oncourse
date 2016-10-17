3fb2112679771dd198efb0612e03e36c50dcd10f
jsoni2 <jakzi7005@gmail.com>
2016-10-17 01:53:12 -0700

    setup oncourse

--- src/main/java/oncourse/model/User.java
+++ src/main/java/oncourse/model/User.java
@@ -23,183 +23,156 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Table(name = "users")
 public class User implements Serializable, UserDetails {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+	public static final String ROLE_ADMIN = "ROLE_ADMIN";
 
-    public static final String ROLE_ADVISOR = "ROLE_ADVISOR";
+	public static final String ROLE_ADVISOR = "ROLE_ADVISOR";
 
-    @Id
-    @GeneratedValue
-    private Long id;
+	@Id
+	@GeneratedValue
+	private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String username;
+	@Column(nullable = false, unique = true)
+	private String username;
 
-    @Column(nullable = false)
-    private String password;
+	@Column(nullable = false)
+	private String password;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+	@Column(nullable = false, unique = true)
+	private String email;
 
-    @Column(nullable = false, unique = true)
-    private String cin;
+	@Column(nullable = false, unique = true)
+	private String cin;
 
-    @Column(name = "first_name")
-    private String firstName;
+	@Column(name = "first_name")
+	private String firstName;
 
-    @Column(name = "last_name")
-    private String lastName;
+	@Column(name = "last_name")
+	private String lastName;
 
-    @ManyToOne
-    @JoinColumn(name = "program_id")
-    private Program program;
+	@ManyToOne
+	@JoinColumn(name = "program_id")
+	private Program program;
 
-    @ElementCollection
-    @CollectionTable(name = "authorities",
-        joinColumns = @JoinColumn(name = "user_id"))
-    @Column(name = "role")
-    private Set<String> roles;
+	@ElementCollection
+	@CollectionTable(name = "authorities", joinColumns = @JoinColumn(name = "user_id"))
+	@Column(name = "role")
+	private Set<String> roles;
 
-    @Column(nullable = false)
-    private boolean enabled;
+	@Column(nullable = false)
+	private boolean enabled;
 
-    public User()
-    {
-        roles = new HashSet<String>();
-        enabled = true;
-    }
+	public User() {
+		roles = new HashSet<String>();
+		enabled = true;
+	}
 
-    public String getName()
-    {
-        return firstName + " " + lastName;
-    }
+	public String getName() {
+		return firstName + " " + lastName;
+	}
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities()
-    {
-        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
-        for( String role : roles )
-            authorities.add( new SimpleGrantedAuthority( role ) );
-        return authorities;
-    }
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+		for (String role : roles)
+			authorities.add(new SimpleGrantedAuthority(role));
+		return authorities;
+	}
 
-    @Override
-    public boolean isAccountNonExpired()
-    {
-        return true;
-    }
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
 
-    @Override
-    public boolean isAccountNonLocked()
-    {
-        return true;
-    }
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
 
-    @Override
-    public boolean isCredentialsNonExpired()
-    {
-        return true;
-    }
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
 
-    public Long getId()
-    {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId( Long id )
-    {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getUsername()
-    {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername( String username )
-    {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getPassword()
-    {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword( String password )
-    {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public String getEmail()
-    {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail( String email )
-    {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getCin()
-    {
-        return cin;
-    }
+	public String getCin() {
+		return cin;
+	}
 
-    public void setCin( String cin )
-    {
-        this.cin = cin;
-    }
+	public void setCin(String cin) {
+		this.cin = cin;
+	}
 
-    public String getFirstName()
-    {
-        return firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName( String firstName )
-    {
-        this.firstName = firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getLastName()
-    {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName( String lastName )
-    {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public Program getProgram()
-    {
-        return program;
-    }
+	public Program getProgram() {
+		return program;
+	}
 
-    public void setProgram( Program program )
-    {
-        this.program = program;
-    }
+	public void setProgram(Program program) {
+		this.program = program;
+	}
 
-    public Set<String> getRoles()
-    {
-        return roles;
-    }
+	public Set<String> getRoles() {
+		return roles;
+	}
 
-    public void setRoles( Set<String> roles )
-    {
-        this.roles = roles;
-    }
+	public void setRoles(Set<String> roles) {
+		this.roles = roles;
+	}
 
-    public boolean isEnabled()
-    {
-        return enabled;
-    }
+	public boolean isEnabled() {
+		return enabled;
+	}
 
-    public void setEnabled( boolean enabled )
-    {
-        this.enabled = enabled;
-    }
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
 
 }
